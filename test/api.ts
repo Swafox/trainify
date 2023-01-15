be438c3cb39b14d20d44d108a629bf4505ae84d7
Swafox <swafox@pm.me>
2023-01-15 12:02:01 +0200

    Update README and init test cases

---
+++ test/api.ts
@@ -0,0 +1,8 @@
+const { moderation } = await import("../openai/moderation.ts");
+
+Deno.test("Moderation identifies no hate", async () => {
+    const response = await moderation("This is a test");
+    if (response["data"]["results"][0]["categories"].hate !== false) {
+        throw Error("hate should be false");
+    }
+});
